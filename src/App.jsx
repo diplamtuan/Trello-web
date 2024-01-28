627391f5f98836300b07db659560321e7f19428b
diplamtuan <85506794+diplamtuan@users.noreply.github.com>
2024-01-28 14:30:09 +0700

    Upload ExtendTheme

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import Button from "@mui/material/Button";
 import AccessTimeFilledIcon from "@mui/icons-material/AccessTimeFilled";
+import { useColorScheme } from "@mui/material/styles";
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme();
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === "light" ? "dark" : "light");
+      }}
+    >
+      {mode === "light" ? "Turn dark" : "Turn light"}
+    </Button>
+  );
+}
 function App() {
   return (
     <>
+      <ModeToggle />
       <div>DipLamTuan</div>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
--- src/theme.js
+++ src/theme.js
@@ -1,10 +1,12 @@
-import { createTheme } from "@mui/material/styles";
+import { experimental_extendTheme as extendTheme } from "@mui/material/styles";
 import { red } from "@mui/material/colors";
-export const theme = createTheme({
-  palette: {
-    mode: "dark",
-    primary: {
-      main: red[500],
+export const theme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: {},
+    },
+    dark: {
+      palette: {},
     },
   },
 });
