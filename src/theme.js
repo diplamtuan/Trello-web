3c3b28e211d84ddf5de46c8f23f22c07e83d4104
diplamtuan <85506794+diplamtuan@users.noreply.github.com>
2024-01-31 17:45:32 +0700

    Upload Header #2

--- src/theme.js
+++ src/theme.js
@@ -1,20 +1,67 @@
 import { experimental_extendTheme as extendTheme } from "@mui/material/styles";
-import { red } from "@mui/material/colors";
+import { outlinedInputClasses } from "@mui/material/OutlinedInput";
 export const theme = extendTheme({
   trello: {
     appHeaderHeight: "58px",
     appBoardHeight: "60px",
   },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: "none",
+        },
+      },
+    },
+    MuiTextField: {
+      styleOverrides: {
+        root: ({ theme }) => ({
+          "& label": {
+            color: theme.palette.primary.main,
+          },
+        }),
+      },
+    },
+    MuiInputLabel: {
+      styleOverrides: {
+        root: ({ theme }) => ({
+          color: theme.palette.primary.main,
+        }),
+      },
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        notchedOutline: ({ theme }) => ({
+          borderColor: theme.palette.primary.main,
+        }),
+        root: ({ theme }) => ({
+          [`&:hover .${outlinedInputClasses.notchedOutline}`]: {
+            borderColor: theme.palette.primary.main,
+          },
+          [`&.Mui-focused .${outlinedInputClasses.notchedOutline}`]: {
+            borderColor: theme.palette.primary.main,
+            borderWidth: "1px",
+          },
+          color: theme.palette.primary.main,
+          fontSize: "0.85rem",
+        }),
+      },
+    },
+  },
   colorSchemes: {
     light: {
       palette: {
         primary: {
-          main: "#ff5252",
+          main: "#45FFCA",
         },
       },
     },
     dark: {
-      palette: {},
+      palette: {
+        primary: {
+          main: "#ED7D31",
+        },
+      },
     },
   },
 });
